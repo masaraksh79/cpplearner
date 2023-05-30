53ed72da1c86d12241520347dc5322ceb6349aa0
Yakir Matusovsky <webthreebiz@gmail.com>
2023-05-30 15:19:32 +1200

    Full completion of last assignment

--- CPPFiles/MoreFiles/stringName_stub.cpp
+++ CPPFiles/MoreFiles/stringName_stub.cpp
@@ -24,18 +24,25 @@ int main()
    std::cout << name.substr(0, 1) << std::endl;
 #endif
 
-#if 0 /* 3-5-4 */
+#if 1 /* 3-5-4 */
    std::cout << "Please enter your Full name:";
    std::getline(std::cin, name);
-   std::cout << name.substr(0, 1);
+
+   // Printing initials
+   std::cout << "You initials are (" << name.substr(0, 1);
    int pos = name.find(" ");
    if (pos == std::string::npos)
    {
       std::cout << "Your full name is of illegal format!\n";
       return 1;
    }
-   std::cout << ' ' << name.substr(pos+1, 1) << std::endl;
+   std::cout << ' ' << name.substr(pos+1, 1) << ')' << std::endl;
    
+   // Printing the last name,first name
+   int len = name.length();
+   assert(pos > 0);
+   std::cout << "You full name is " << name.substr(pos+1, len-pos+1) 
+             << "," << name.substr(0, pos) << std::endl;
 #endif
 
     return 0;
