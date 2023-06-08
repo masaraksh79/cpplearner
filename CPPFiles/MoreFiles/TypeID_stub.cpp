b076ba39b2eecb5b5da3979a8afe1a5c91be1a85
Yakir Matusovsky <webthreebiz@gmail.com>
2023-06-08 13:33:33 +1200

    typeID 7-8 exercises

--- CPPFiles/MoreFiles/TypeID_stub.cpp
+++ CPPFiles/MoreFiles/TypeID_stub.cpp
@@ -6,6 +6,11 @@
 #include <typeinfo>
 #include <string>
 
+using std::cout;
+using std::endl;
+
+class MyClass{ private: int i; public: MyClass():i{0}{}};
+
 // ------------------------------------
 int main()
 {
@@ -17,6 +22,16 @@ int main()
     float f;
     double d;
     std::string s;
+
+    cout << "b=" << typeid(b).name() << '\n';
+    cout << "c=" << typeid(c).name() << '\n';
+    cout << "sh=" << typeid(sh).name() << '\n';
+    cout << "i=" << typeid(i).name() << '\n';
+    cout << "l=" << typeid(l).name() << '\n';
+    cout << "f=" << typeid(f).name() << '\n';
+    cout << "d=" << typeid(d).name() << '\n';
+    cout << "myClass=" << typeid(MyClass).name() << '\n';
+    cout << "s=" << typeid(s).name() << endl;
 
     return 0;
 }
